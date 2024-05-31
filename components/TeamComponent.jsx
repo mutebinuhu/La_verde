d0be4dfdac8fdd2cde0cb2b96f601b22ab05ed33
mutebinuhu <mutebinuhu1@gmail.com>
2024-05-31 16:31:22 +0400

    chore: modify about us page

--- components/TeamComponent.jsx
+++ components/TeamComponent.jsx
@@ -1,7 +1,7 @@
 // TeamComponent.jsx
 import React from 'react';
 
-const teamMembers = [
+const ceo =[
   {
     name: 'Ahmad youssef',
     position: 'CEO',
@@ -11,15 +11,8 @@ const teamMembers = [
       linkedin: 'https://linkedin.com/in/johndoe',
     },
   },
-  {
-    name: 'Hazem',
-    position: 'Sales Executive',
-    image: '/workmates/hr.jpeg',
-    social: {
-      twitter: 'https://twitter.com/janesmith',
-      linkedin: 'https://linkedin.com/in/janesmith',
-    },
-  },
+]
+const teamMembers = [
   {
     name: 'Abdelrahman',
     position: 'Sales Manager',
@@ -36,6 +29,25 @@ const teamMembers = [
     social: {
       twitter: 'https://twitter.com/bobbrown',
       linkedin: 'https://linkedin.com/in/bobbrown',
+    },
+  },
+  {
+    name: 'Hazem',
+    position: 'Sales Executive',
+    image: '/workmates/hr.jpeg',
+    social: {
+      twitter: 'https://twitter.com/janesmith',
+      linkedin: 'https://linkedin.com/in/janesmith',
+    },
+  },
+  
+  {
+    name: 'Amir',
+    position: 'Property Sales Consultant',
+    image: '/workmates/samir.jpeg',
+    social: {
+      twitter: 'https://twitter.com/charliegreen',
+      linkedin: 'https://linkedin.com/in/charliegreen',
     },
   },
   {
@@ -70,6 +82,45 @@ const TeamComponent = () => {
         </div>
 
         <div className="mt-10">
+            <div className='md:flex w-full py-12 justify-center'>
+            <div  className="bg-white shadow-lg rounded-lg overflow-hidden">
+                <img
+                  className="w-full h-96 object-cover object-center"
+                  src={ceo[0].image}
+                  alt={ceo[0].name}
+                />
+                <div className="p-6">
+                  <h3 className="text-lg font-semibold text-gray-900">{ceo[0].name}</h3>
+                  <p className="text-indigo-600">{ceo[0].position}</p>
+                  <div className="mt-4">
+                    <a
+                     /** member.social.twitter */
+                      href={"#"}
+                      className="text-gray-500 hover:text-gray-900 mr-4"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span className="sr-only">Twitter</span>
+                      <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M24 4.56c-.89.39-1.84.66-2.84.78a4.92 4.92 0 002.16-2.72c-.95.56-2.02.97-3.15 1.19a4.92 4.92 0 00-8.39 4.49c-4.09-.2-7.72-2.17-10.14-5.15a4.93 4.93 0 001.52 6.57A4.87 4.87 0 01.96 9v.06a4.92 4.92 0 003.95 4.83 4.92 4.92 0 01-2.21.08 4.93 4.93 0 004.6 3.42A9.86 9.86 0 010 21.54a13.93 13.93 0 007.56 2.22c9.05 0 14-7.5 14-14 0-.21 0-.42-.01-.63a9.98 9.98 0 002.45-2.54l.02-.03z" />
+                      </svg>
+                    </a>
+                    <a
+                    /**member.social.linkedin**/
+                      href={"#"}
+                      className="text-gray-500 hover:text-gray-900"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span className="sr-only">LinkedIn</span>
+                      <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M20.447 20.452h-3.558v-5.569c0-1.328-.027-3.036-1.85-3.036-1.851 0-2.135 1.445-2.135 2.94v5.665H9.34V9.998h3.414v1.432h.047c.477-.9 1.637-1.85 3.37-1.85 3.603 0 4.268 2.37 4.268 5.454v6.418h-.002zM5.337 8.532a2.065 2.065 0 01-2.065-2.065 2.065 2.065 0 112.065 2.065zm-1.78 11.92h3.558V9.998H3.558v10.454zm16.822-17.903H3.421C2.56 2.548 2 3.112 2 3.89v16.229c0 .777.56 1.35 1.421 1.35h16.962c.86 0 1.421-.573 1.421-1.35V3.89c0-.778-.561-1.342-1.422-1.342z" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {teamMembers.map((member, index) => (
               <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden">
