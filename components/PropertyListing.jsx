7167450a7655f5292b25abc74151b3c0d30be193
mutebinuhu <mutebinuhu1@gmail.com>
2024-06-03 16:42:11 +0400

    core: few changes

--- components/PropertyListing.jsx
+++ components/PropertyListing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import PropertyDetails from './PropertyDetails';
 async function getData() {
-    const res = await fetch('http://localhost:3000/api/properties')
+    const res = await fetch('https://laverde.ae/api/properties')
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
     const data = await res.json();
@@ -21,7 +21,7 @@ const PropertyListing = async () => {
 
   return (
     <div className=" mt-10">
-      <h1 className="text-3xl font-bold mb-5 text-center">Property Listings page</h1>
+      <h1 className="text-3xl font-bold mb-5 text-center">Property Listings pages</h1>
       <div className="">
         {
             properties.data.map(property => (
