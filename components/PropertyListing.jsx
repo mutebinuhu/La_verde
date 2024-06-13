b0eb58b2e26e73bc8f71d8c51bad38be35af0c35
mutebinuhu <mutebinuhu1@gmail.com>
2024-06-13 17:37:13 +0400

    chore: few changes

--- components/PropertyListing.jsx
+++ components/PropertyListing.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import PropertyDetails from './PropertyDetails';
 async function getData() {
 
-    const res = await fetch('http://localhost:3000/api/properties')
+    const res = await fetch(process.env.API_URL+"api/properties")
+    console.log("test====", res);
     
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
