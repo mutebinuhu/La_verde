ef9557474ee3198a9b808bf41623aea207b67994
mutebinuhu <mutebinuhu1@gmail.com>
2024-06-22 13:58:19 +0400

    core: few changes

--- components/Filters.js
+++ components/Filters.js
@@ -12,6 +12,7 @@ const Filters = ({ onFilterChange }) => {
     minPrice: '',
     maxPrice: '',
     bedrooms: '',
+    location:'',
   });
 
   const handleChange = (e) => {
@@ -118,7 +119,7 @@ const Filters = ({ onFilterChange }) => {
         </div>
         <div className="w-full">
           <label className="block text-sm font-medium text-gray-700">Location</label>
-            <select type='' className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'>
+            <select type='' name='location' onChange={handleChange} className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'>
             <option value="" label="Select Area" className='' />
 
                   {locationList && locationList.map((loca)=>  <option className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' value={loca.name} label={loca.name} />)}
