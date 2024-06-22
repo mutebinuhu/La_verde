4acbdd4f74f47150deea75079725c534c39be837
mutebinuhu <mutebinuhu1@gmail.com>
2024-06-22 13:41:53 +0400

    chore: few changes

--- app/components/Reusables/PopularPlaces.jsx
+++ app/components/Reusables/PopularPlaces.jsx
@@ -6,7 +6,7 @@ import PopularServicesComponent from "./PopularServicesComponent";
 
 const PopularPlaces = () =>{
     return(
-        <section className="container mx-auto mx-4 mt-20 md:-mt-40 tex-center mb-96 md:mb-0">
+        <section className="container mx-auto mx-4 mt-32 md:-mt-32 tex-center mb-96 md:mb-0">
             <h2 className={`${nonoSerif.className} text-4xl text-bold text-center py-8 mx-4`}>Most Popular Areas In Abudhabi</h2>
             <p className='text-center py-8 text-xl'>Own a home in one of the most popular and luxurious places in Abudhabi. Get yourself a home in <strong>Reem Island,</strong> <strong>Yas Island</strong>, <strong>Sadiyaat  Island, Al Maryah Island, Al Raha, Al Hudayriat Island</strong> </p>
            < PopularServicesComponent/>
