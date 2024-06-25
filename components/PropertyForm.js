671a2267b0c6865b34cb07b3bf24f09da4f290eb
mutebinuhu <mutebinuhu1@gmail.com>
2024-06-25 19:04:21 +0400

    core: few changes

--- components/PropertyForm.js
+++ components/PropertyForm.js
@@ -322,7 +322,7 @@ const PropertyForm = () => {
               )}
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 py-4">Amenities</label>
+              <label className="block text-lg font-bold  text-gray-700 py-4">Amenities</label>
               <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {amenitiesList.map((amenity, index) => (
                   <label key={index} className="flex items-center">
