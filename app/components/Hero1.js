2d0efe134eed7bec175af92ef229dcbef7a2a4f9
mutebinuhu <mutebinuhu1@gmail.com>
2024-08-13 19:10:04 +0400

    chore: work on the header page

--- app/components/Hero1.js
+++ app/components/Hero1.js
@@ -13,19 +13,19 @@ const  ButtonGroup = ()=>{
       <div className="flex ">
         <button
           onClick={() => handleClick('buy')}
-          className={`px-12 py-6 font-semibold rounded-tl-xl  ${activeButton === 'buy' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={`px-20 py-6 font-semibold rounded-tl-xl  ${activeButton === 'buy' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
         >
           Buy
         </button>
         <button
           onClick={() => handleClick('sell')}
-          className={`px-12 py-6 font-semibold  ${activeButton === 'sell' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={`px-20 py-6 font-semibold  ${activeButton === 'sell' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'}`}
         >
           Sell
         </button>
         <button
           onClick={() => handleClick('rent')}
-          className={`px-12 py-6 font-semibold rounded-tr-xl ${activeButton === 'rent' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={`px-32 py-6 font-semibold rounded-tr-xl ${activeButton === 'rent' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-700'}`}
         >
           Rent
         </button>
@@ -37,15 +37,15 @@ const Hero1 = () =>{
         <>
             
         
-            <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Looking for a Home in Abu Dhabi ?</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Search Confidently With Your trusted Source For Homes to Buy, Rent, Sell in Abu Dhabi for reasonable Price</p>
+            <section class="bg-green-300 dark:bg-gray-900">
+    <div class="grid max-w-screen-xl md:px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto  place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 p-2 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Looking for a Home in Abu Dhabi ?</h1>
+            <p class="max-w-2xl mb-6 p-2 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Search Confidently With Your trusted Source For Homes to Buy, Rent, Sell in Abu Dhabi for reasonable Price</p>
          
            <PropertySearchForm/>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex mx-2">
             <img src="imageedit_2_2051960956-400x250.webp" alt="mockup" />
         </div>                
     </div>
--- app/components/PropertySearchForm.js
+++ app/components/PropertySearchForm.js
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 
 export default function PropertySearchForm() {
-  const [activeButton, setActiveButton] = useState('');
+  const [location, setLocation] = useState('');
+
+  const [activeButton, setActiveButton] = useState('rent');
+  const [locationList, setLocationList] = useState([])
+
   const [formData, setFormData] = useState({
     location: '',
     propertyType: '',
@@ -22,72 +27,107 @@ export default function PropertySearchForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const query = {};
+    if (formData.propertyType) query.subCategory = formData.propertyType;
+    if (formData.location) query.location = formData.location;
+    if (formData.maxPrice) query.maxPrice = formData.maxPrice;
+    if (activeButton) query.purpose = activeButton;
+
+    console.log("query", query);
+
     // Handle form submission, e.g., API call with formData and activeButton
     console.log('Form submitted:', formData, 'Type:', activeButton);
+
   };
 
+  const getLocations = async () =>{
+    try {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/locations')
+      const data = await res.json();
+      setLocationList(data.data)
+      console.log("locations", data)
+    } catch (error) {
+      console.log("locerr", error)
+    }
+  }
+  useEffect(()=>{
+    getLocations();
+  }, [])
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="flex ">
+    <form onSubmit={handleSubmit} className="">
+      <div className="flex">
+        <button
+          type="button"
+          onClick={() => handleButtonClick('rent')}
+          className={`px-12 md:px-12 py-6 font-semibold  bg-blue-500 rounded-tl-xl  text-white`}
+        >
+          Rent
+        </button>
         <button
           type="button"
           onClick={() => handleButtonClick('buy')}
-          className={`px-10 md:px-12 py-6 font-semibold rounded-tl-xl  ${activeButton === 'buy' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={`px-12 py-6 font-semibold ${activeButton === 'buy' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'}`}
         >
           Buy
         </button>
         <button
           type="button"
           onClick={() => handleButtonClick('sell')}
-          className={`px-10 py-6 font-semibold ${activeButton === 'sell' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={`px-12 py-6 font-semibold rounded-tr-xl ${activeButton === 'sell' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-700'}`}
         >
           Sell
         </button>
-        <button
-          type="button"
-          onClick={() => handleButtonClick('rent')}
-          className={`px-10 py-6 font-semibold rounded-tr-xl ${activeButton === 'rent' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-        >
-          Rent
-        </button>
       </div>
 
-      <div className="flex w-72 ">
-        <input
+      <div className="bg-white ">
+          <div className='flex  w-full '>
+          <select
           type="text"
           name="location"
           placeholder="Location"
           value={formData.location}
           onChange={handleInputChange}
-          className="p-2 border rounded"
-        />
-        <input
+          className="p-2  w-24 md:w-full rounded"
+        >
+          <option value="">Location</option>
+            {locationList && locationList.map((loca)=>  <option className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' value={loca.name} label={loca.name} />)}
+       
+        </select>
+        <select
           type="text"
           name="propertyType"
-          placeholder="Property Type"
+          placeholder="Type"
           value={formData.propertyType}
           onChange={handleInputChange}
-          className="p-2 border rounded"
-        />
+          className="p-2  w-24 md:w-full rounded"
+        >
+           <option value="">Type</option>
+          <option value="villa">Villa</option>
+          <option value="studio">Studio</option>
+          <option value="apartment">Apartment</option>
+          <option value="residentialFloor">Residential Floor</option>
+          <option value="residentialPlot">Residential Plot</option>
+          <option value="townHouse">Town House</option>
+          <option value="residentialBuilding">Residential Building</option>
+          <option value="pentHouse">Pent House</option>
+          <option value="villaCompound">Villa Compound</option>
+          </select>
         <input
           type="number"
           name="maxPrice"
           placeholder="Max Price"
           value={formData.maxPrice}
           onChange={handleInputChange}
-          className="p-2 border rounded"
+          className="p-2 w-24 md:w-full rounded"
         />
-        <button
-        type="submit"
-        className="flex items-center justify-center px-4 py-2 bg-blue-500 text-white font-semibold rounded"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M13 7H7v2h6V7zM7 9v2h6V9H7zm0 2v2h6v-2H7zm2-6h2V2H9v3zm7.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L9 12.586V2a1 1 0 112 0v10.586l3.293-3.293a1 1 0 011.414 0z" clipRule="evenodd" />
-        </svg>
-        Search
-      </button>
+      <div className='w-full'>
+      <button className='bg-blue-500 md:p-6 p-4 w-full   text-white'>
+          Go
+        </button>
       </div>
-
+          </div>
+        </div>
+        
       
     </form>
   );
