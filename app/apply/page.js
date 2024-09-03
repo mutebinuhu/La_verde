6d8a9f72682a663f2c53f063ff2116985d4cf8cd
mutebinuhu <mutebinuhu1@gmail.com>
2024-09-03 11:46:46 +0400

    chore: bug fix

--- app/apply/page.js
+++ app/apply/page.js
@@ -46,7 +46,7 @@ export default function Home() {
     console.log("form data", formData)
 
     try {
-      const response = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/applications', {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL+'api/applications', {
         method:"POST",
         body:form
       });
