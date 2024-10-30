c1cc96818c38881a5d35570764a1a7b7a6f91778
mutebinuhu <mutebinuhu1@gmail.com>
2024-10-30 18:09:26 +0400

    core: change the contact us page

--- components/ContactUsMain.jsx
+++ components/ContactUsMain.jsx
@@ -74,8 +74,72 @@ export default function ContactUsMain() {
             <p class="mt-3 text-gray-500 dark:text-gray-400">We’d love to hear from you. Please fill out this form or shoot us an email.</p>
         </div>
 
-        <div class="grid grid-cols-1 gap-12 mt-10 lg:grid-cols-2">
-            <div class="grid grid-cols-1 gap-12 md:grid-cols-2">
+        <div class="grid grid-cols-1 gap-12 mt-10 lg:grid-cols-2 bg-gray-50 dark:bg-gray-800">
+        <div class="p-4 py-6 rounded-lg  md:p-8">
+            <form onSubmit={handleSubmit}>
+                <div className="-mx-2 md:items-center md:flex">
+                  <div className="flex-1 px-2">
+                    <label className="block mb-2 text-sm  font-semibold text-gray-600 dark:text-gray-200">First Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                    {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
+                  </div>
+
+                  <div className="flex-1 px-2 mt-4 md:mt-0">
+                  <div className="">
+                  <label className="block mb-2 text-sm font-semibold text-gray-600 dark:text-gray-200">Phone</label>
+                  <input
+                    type="text"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                  {errors.phoneNumber && <p className="text-red-500 text-sm">{errors.phoneNumber}</p>}
+                </div>
+                  </div>
+                </div>
+
+                <div className="mt-4">
+                  <label className="block mb-2 text-sm font-semibold text-gray-600 dark:text-gray-200">Email address</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                  {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+                </div>
+
+          
+
+                <div className="w-full mt-4">
+                  <label className="block mb-2 text-sm font-semibold text-gray-600 dark:text-gray-200">Message</label>
+                  <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    className="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Message"
+                  ></textarea>
+                  {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full px-6 py-3 font-semibold mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#104E3E] rounded-lg hover:bg-[#4c7a6e] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                >
+                  Send message
+                </button>
+              </form>
+            </div>
+            <div class="grid grid-cols-1 gap-12 md:grid-cols-2 md:py-8 px-4 md:px-0">
                 <div>
                     <span class="inline-block p-3 text-[#104E3E] rounded-full bg-[#bec7c4] dark:bg-gray-800">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -127,70 +191,7 @@ export default function ContactUsMain() {
                 </div>
             </div>
 
-            <div class="p-4 py-6 rounded-lg bg-gray-50 dark:bg-gray-800 md:p-8">
-            <form onSubmit={handleSubmit}>
-                <div className="-mx-2 md:items-center md:flex">
-                  <div className="flex-1 px-2">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">First Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      value={formData.name}
-                      onChange={handleChange}
-                      className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                    />
-                    {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
-                  </div>
-
-                  <div className="flex-1 px-2 mt-4 md:mt-0">
-                  <div className="">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Phone</label>
-                  <input
-                    type="text"
-                    name="phoneNumber"
-                    value={formData.phoneNumber}
-                    onChange={handleChange}
-                    className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                  />
-                  {errors.phoneNumber && <p className="text-red-500 text-sm">{errors.phoneNumber}</p>}
-                </div>
-                  </div>
-                </div>
-
-                <div className="mt-4">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                  />
-                  {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-                </div>
-
-          
-
-                <div className="w-full mt-4">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                  <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    className="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Message"
-                  ></textarea>
-                  {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full px-6 py-3 mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#104E3E] rounded-lg hover:bg-[#4c7a6e] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-                >
-                  Send message
-                </button>
-              </form>
-            </div>
+         
         </div>
     </div>
 </section>
